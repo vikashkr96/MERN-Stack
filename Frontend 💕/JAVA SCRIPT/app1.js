7657c08b0da454d014696d4957609e7a2a6b89ab
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-10 06:04:53 +0530

    Add files via upload

---
+++ Frontend 💕/JAVA SCRIPT/app1.js
@@ -0,0 +1,11 @@
+//  Create a system to calculate popcorn prices based on the size customer asked for : 
+let size = "L";
+if (size === "XL"){
+    console.log("The price of popcorn is RS.250")
+}else if(size === "L"){
+    console.log("The price of popcorn is RS.200")
+}else if(size === "M"){
+    console.log("The price of popcorn is RS.100")
+}else if(size === "S"){
+    console.log("The price of popcorn is RS.50")
+}
