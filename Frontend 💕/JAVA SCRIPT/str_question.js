3921006b32d74c2432dcaf8b6d891216d1950f03
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-15 23:21:02 +0530

    Add files via upload

---
+++ Frontend 💕/JAVA SCRIPT/str_question.js
@@ -0,0 +1,11 @@
+// for given srting  trim it and convert it into uppercase 
+
+let msg = "help!";
+console.log(msg.trim().toUpperCase());
+
+// predict the output 
+
+let name = "ApnaCollege"
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
