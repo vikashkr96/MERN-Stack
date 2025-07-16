35867bca4ebac8c04bf4c0e6e4024411156de8fb
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-16 23:43:37 +0530

    Add files via upload

---
+++ Frontend 💕/JAVA SCRIPT/array.js
@@ -0,0 +1,45 @@
+let students = ["Aman" , "Vikash" , "Amit"];
+console.log(students);
+console.log(students);
+console.log(students[2]);
+
+// arrays are mutable 
+
+fruits = ["manago", "apple" , "guava"];
+console.log(fruits);
+fruits[0] = "Banana";
+console.log(fruits);
+
+a = ["aaloo","tamatar"];
+a[10] = "gajar";
+console.log(a);
+
+// array methods in js
+
+let arr = ["a" , "b" , "c"];
+console.log(arr.push("d"));
+console.log(arr);
+console.log(arr.unshift("0"));
+console.log(arr);
+console.log(arr.shift());
+console.log(arr);
+console.log(arr.pop());
+console.log(arr);
+
+let colors = ["red","yellow","blue"];
+console.log(colors.indexOf("red"));
+console.log(colors.indexOf("green"));
+console.log(colors.includes("green"));
+
+let pr = ["blue" , "Yellow" , "Green"];
+let se = ["Violet" , "Pink" , "White"];
+console.log(pr.concat(se));
+console.log(pr.concat(se).reverse())
+
+//slice method yes done ......
+
+
+
+
+
+
