f7ff3362e29ec90b44a0e2f96b0b5a05633e4a76
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-20 23:44:49 +0530

    Add files via upload

---
+++ Frontend 💕/JAVA SCRIPT/assignment3.js
@@ -0,0 +1,48 @@
+//(1) Write a JavaScript program to get the first n elements of an array. 
+// [n can be any positive number].
+// For example:for array [7,9,0,-2 ]and n=3 
+// Print ,[7,9,0]
+// let arr = [7,9,0,-2];
+// let n = 3;
+// console.log(arr.slice(0,n));
+
+
+// Qs2.Write a JavaScript program to get the last n elements of an array.
+// [n can be any positive number].
+// For example:for array [7,9,0,-2] and n=3 , Print [9,0,-2]
+// let arr1 = [7,9,0,-2];
+// let n1 = 3;
+// console.log(arr1.slice(arr1.length-n1));
+
+//Qs3.Write a JavaScript program to check whether a string is blank or not
+
+// let str = prompt("Enter a string : ")
+// if (str.length == 0){
+//     console.log("String is empty");
+
+// }else{
+//     console.log("String is not empty");
+// }
+
+// Qs6.Write a JavaScript program to check if an element exists in an array or not.
+
+ar = ['vikash','nasir','ratan','nandu','nik'];
+elm = 'nandu';
+
+if (ar.indexOf(elm) != -1){
+    console.log("Found!");
+}else{
+    console.log(" Not Found!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
