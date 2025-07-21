739492441d8317662d7911e52cf9443f24f46f37
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-21 23:48:28 +0530

    Add files via upload

---
+++ Frontend 💕/JAVA SCRIPT/forloop.js
@@ -0,0 +1,38 @@
+for (let i = 1 ; i<10 ; i++){
+    console.log(i);
+}
+
+for (let j = 10; j>=0 ; j-=2){
+    console.log(j);
+}
+
+// odd nums from 1 to 15 
+for ( let i = 1 ; i<=15 ; i+=2){
+    console.log(i)
+}
+
+for ( let i = 15 ; i>=1 ; i-=2){
+    console.log(i)
+}
+
+// even nums from1 to 10
+
+for (let i = 1 ; i <= 10 ; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+//infinite loop
+
+// for ( let i = 1 ; i>=1 ; i++){
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
