e5ce7110a15b5bd8549e1984cce0e74d91ecaf31
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-22 08:30:30 +0530

    Add files via upload

---
+++ Frontend 💕/JAVA SCRIPT/loops_with_array.js
@@ -0,0 +1,9 @@
+let fruits = ["Mango", "Apple" , "Banana" , "Guava"];
+
+// for (let i = 0; i<= fruits.length ;i++){
+//     console.log(i,fruits[i]);
+// }
+
+for (let i = fruits.length-1 ; i>= 0 ; i--){
+    console.log(i,fruits[i]);
+}
