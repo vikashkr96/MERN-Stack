28590e5d261621fbeebcb1a4d5ee9439ed338773
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-22 08:31:22 +0530

    Add files via upload

---
+++ Frontend 💕/while_loop.js
@@ -0,0 +1,21 @@
+let i = 7;
+while(i<=70){
+    if(i%7==0){
+        console.log(i);
+    }
+    i+=1;
+}
+
+// movie guessing game using while loop
+
+// const fav = "avengers";
+// guess = prompt("Guess my favourite movie name: ");
+
+// while((guess!=fav) || (guess!="quit")){
+//     guess = prompt("Guess my favourite movie name again : "); 
+// }
+// if (guess == fav ){
+//     console.log("You guessed it right !")
+// }else{
+//     console.log("You quit !")
+// }
