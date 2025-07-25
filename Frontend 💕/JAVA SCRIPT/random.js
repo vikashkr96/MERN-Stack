4b60e7deffe566a0004aa1f9dd543d086f992f40
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-25 07:36:01 +0530

    Add files via upload

---
+++ Frontend 💕/JAVA SCRIPT/random.js
@@ -0,0 +1,17 @@
+// random numbers from 1 to 10
+
+let num = Math.random();
+num = num*10;
+num = Math.floor(num);
+num = num+1;
+console.log(num);
+
+// random numbers from 1 to 100
+
+let random = Math.floor(Math.random()*100)+1;
+console.log(random);
+
+// random numbers from 1 to 5
+
+let nums = Math.floor(Math.random()*5)+1;
+console.log(nums);
