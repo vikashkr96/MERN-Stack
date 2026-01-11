c37e85be3ff639bbcecdec663b2f24a854870a65
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-01-12 00:53:54 +0530

    Update _event_delegation.js

--- Frontend 💕/Mini Projects/To Do using DOM/_event_delegation.js
+++ Frontend 💕/Mini Projects/To Do using DOM/_event_delegation.js
@@ -17,19 +17,20 @@ btn.addEventListener("click",function(){
 
 });
 
-// let dltbtns = document.querySelectorAll(".delete");
-// for(dltbtn of dltbtns){
-//     dltbtn.addEventListener("click",function(){
-//         let par = dltbtn.parentElement;
-//         par.remove();
-//     })
-// }; >> this can be a problem for newly added elements though event listeners
+let dltbtns = document.querySelectorAll(".delete");
+for(dltbtn of dltbtns){
+    dltbtn.addEventListener("click",function(){
+        let par = dltbtn.parentElement;
+        par.remove();
+    })
+}; >> //this can be a problem for newly added elements though event listeners
 //       so we will use event delegation and bubbling condition 
 //       ie: we will use event listener on parent element of button instead of using on it 
 
 ul.addEventListener("click",function(){
     console.log("button was clicked"); 
 });  // now this will work on our newly created elements because of event bubbling
+
 
 
 
