6dd02b7d553b26d46efa38d494ea8d6694201358
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-03-04 13:44:07 +0530

    Update index.js

--- Backend/Middlewares/index.js
+++ Backend/Middlewares/index.js
@@ -20,29 +20,56 @@ app.use(cookieParser());
 
 
 // creating a utility middleware - logger
-app.use((req,res,next)=>{
-    console.log(req.hostname);
-    console.log(req.method);
-    console.log(req.path);
-    console.log(req.cookies);
-    next();
-})
+// app.use((req,res,next)=>{
+//     console.log(req.hostname);
+//     console.log(req.method);
+//     console.log(req.path);
+//     console.log(req.cookies);
+//     next();
+// })
 
 // path specific middlewares , and if we not do so , i'll be for all paths then ....
-app.use("/random" , (req, res, next)=>{
-    console.log("I'm  only for random ! ");
+// app.use("/random" , (req, res, next)=>{
+//     console.log("I'm  only for random ! ");
+// })
+
+
+// creating a middleware for an api to check if the access token was passed in the query string or not
+// app.use("/api",(req, res,next)=>{
+//     let {token} = req.query;
+//     if(token === "giveaccess"){
+//         next();
+//     }
+//     res.send("ACCESS DENIED !");
+// })
+
+// app.get("/api", (req, res)=>{
+//     res.send("data");
+// }) 
+
+// passing multiple middlewares 
+const checkToken = (req, res, next) => {
+    let { token } = req.query;
+
+    if (token === "giveaccess") {
+        return next();
+    }
+
+    return res.send("ACCESS DENIED !");
+};
+
+app.get("/api", checkToken, (req, res) => {
+    res.send("data");
 })
 
-
-
-
-app.get("/", (req, res)=>{
+// home route
+app.get("/",checkToken, (req, res)=>{
     res.send("Home page");
 })
 
-app.get("/random", (req, res)=>{
-    res.send("Its a random page !");
-})
+// app.get("/random", (req, res)=>{
+//     res.send("Its a random page !");
+// })
 
 app.listen(port, ()=>{
     console.log("server started running");
