5d5e11ed42690dfa7f104f420029537fbef4c7ba
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-03-10 20:07:23 +0530

    Update customer.js

--- DataBases/MongoDB/Relationships/models/customer.js
+++ DataBases/MongoDB/Relationships/models/customer.js
@@ -26,6 +26,18 @@ const customerSchema = mongoose.Schema({
     ],
 });
 
+// to tackle this issue - " when customer is deleted his order rmains in the db we will use pre and post middlewares "
+// customerSchema.pre("findOneAndDelete", async()=>{
+//     console.log("pre middleware ! ");
+// });
+
+// here we need post middleware 
+customerSchema.post("findOneAndDelete", async(customer)=>{
+    if(customer.orders.length){
+        result = await Order.deleteMany({_id:{$in:customer.orders}});
+        console.log(result);
+    }
+});
 
 
 
@@ -34,25 +46,84 @@ const Order = mongoose.model("Order",orderSchema);
 const Customer = mongoose.model("Customer",customerSchema);
 
 
-//const addCustomer = async()=>{
-//    let cust1 = new Customer({
-//        name: 'Vikash Kumar'
-//   });
-//
-//    let order1 = await //Order.findOne({item:"Chips"});
-//    let order2 = await //Order.findOne({item:"Chocolate"});   
 
-//    cust1.orders.push(order1);
-//    cust1.orders.push(order2);  // this will push only the orderid not //exact order details ....
+// Functions 
 
-//    let res = await cust1.save();
-//    console.log(res);
-//}
-
-//addCustomer();
+const findCustomer = async()=>{
+    let result = await Customer.find({}).populate("orders");
+    console.log(result[0]);
+}
 
 
+const addCustomer = async()=>{
+    let newCust = new Customer({
+        name: "Karan Arjun"
+    });
 
+    let newOrder = Order({
+        item: "Litti Chokha",
+        price: 200
+    });
+    newCust.orders.push(newOrder);
+    await newOrder.save();
+    await newCust.save();
+    console.log("Added new customer !");
+    
+}
+
+// addCustomer();     // customer and order added sucessfully  in db // re added karan arjun with Burger this time to check the deletions
+
+// deleting it normally 
+const deleteCustomer = async()=>{
+    let data = await Customer.findByIdAndDelete('69b02b72e20837e3b890c907');
+    console.log(data);
+}
+deleteCustomer();   /// in the db customer is deleted but its order is still there its not deleted 
+   //  Now when a Customer is deleted → Orders also delete automatically.
+
+   // yes its working the litti chokha automatically deleted whene i deleted karan arjun 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// previously added values in db ...........
+// const addCustomer = async()=>{
+//     let cust1 = new Customer({
+//         name: 'Vikash Kumar'
+//     });
+
+//     let order1 = await Order.findOne({item:"Chips"});
+//     let order2 = await Order.findOne({item:"Chocolate"});   
+
+//     cust1.orders.push(order1);
+//     cust1.orders.push(order2);  // this will push only the orderid not exact order details ....
+
+//     let res = await cust1.save();
+//     console.log(res);
+// }
+
+// addCustomer();
 
 
 // const addOrders = async ()=>{
