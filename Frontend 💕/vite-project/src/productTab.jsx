19cae99ed065b7dd915b51da92a08d7df549c483
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-05-09 18:35:07 +0530

    Update productTab.jsx

--- Frontend 💕/vite-project/src/productTab.jsx
+++ Frontend 💕/vite-project/src/productTab.jsx
@@ -10,3 +10,34 @@ function ProductTab() {
   );
 }
 export default ProductTab;
+
+// this is how we pass array / objects and render it through React props.................
+// import Product from "./Product.jsx";
+
+// function ProductTab() {
+//   let features = ["New Launched", "AI Driven System", "Better Performance"];
+//   return (
+//     <>
+//       <Product
+//         title="phone"
+//         price={58800}
+//         description="aaaaa aaaaaaaaa aaaaaaa aaaaaa aaaa aaaa"
+//         features = {features}
+//       />
+
+//       <Product
+//         title="laptop"
+//         price={52000}
+//         description="bbbbbb bbbbbbb bbbbbb bbbbb bbbbbb bbbbb"
+//       />
+
+//       <Product
+//         title="tab"
+//         price={50002}
+//         description="nnnnn nnnnnnnnnn nnnnnn nnn nnnn nnnnnnn"
+//       />
+//     </>
+//   );
+// }
+
+// export default ProductTab;
