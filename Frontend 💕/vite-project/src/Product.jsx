492cdef0c38b94eced4efc5354ee3c1381a162fa
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-05-09 18:43:58 +0530

    Update Product.jsx

--- Frontend 💕/vite-project/src/Product.jsx
+++ Frontend 💕/vite-project/src/Product.jsx
@@ -8,3 +8,17 @@ function Product() {
   );
 }
 export default Product;
+
+// this is how we add conditionals 
+// import "./Product.css";
+
+// export default function Product({ title, price, description }) {
+//   return (
+//     <div className="Product">
+//       <h2>Title: {title}</h2>
+//       <h4>Price: {price}</h4>
+//       {price > 55000 ? <p>Discount of 5%</p> : null}
+//       <p>More info: {description}</p>
+//     </div>
+//   );
+// }
